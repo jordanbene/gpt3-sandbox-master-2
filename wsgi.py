260a0aa9ed878af856d7f340513c5474d8be80de
jordanbene <62028734+jordanbene@users.noreply.github.com>
2023-02-01 22:48:52 -0800

    Update wsgi.py
    public folder/send directory instead of static if app and script not same dir

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
-from flask import request, url_for
+from flask import request, url_for, send_from_directory
 import api.text_adventure
 text = "./api/text_adventure.py" 
 from flask import Flask
 from api import text_adventure
-
+import os 
 app = text_adventure.app
-app.static_folder = 'public'
+app.static_folder = './public/'
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -14,4 +14,5 @@ if __name__ == '__main__':
 def index():
     '''Index page route'''
     #return app.send_static_file('public/index.html')
-    return app.send_static_file(url_for('static', filename='index.html'))
+    #return app.send_static_file(url_for('static', filename='index.html'))
+    return send_from_directory(app.static_folder, 'index.html')
