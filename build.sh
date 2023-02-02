a6f32a6030a5050b7ab6eee9b4b1c12c24eddcc8
jordanbene <62028734+jordanbene@users.noreply.github.com>
2023-02-02 10:49:38 -0800

    Update build.sh
    build 2

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -o errexit
 pip install --upgrade pip
- pip install gunicorn
- pip install -r requirements.txt
- npm install; yarn build
+pip install gunicorn
+pip install -r requirements.txt
+npm install yarn build
